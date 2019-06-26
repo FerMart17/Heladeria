de741dd2bc1e2240abefc02bef85014d1d633702
BrendaMendez17 <51276729+BrendaMendez17@users.noreply.github.com>
2019-06-26 19:40:00 -0300

    Add files via upload
    aver :V

---
+++ UsuarioBean.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apliacion.form.bean;
+
+import aplicacion.dao.IUsuarioDAO;
+import aplicacion.dao.mysql.UsuarioDaoImp;
+import aplicacion.modelo.dominio.Usuario;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author BRENDA
+ */
+@ManagedBean
+@ViewScoped
+public class UsuarioBean implements Serializable{
+    private List<Usuario> listaUsarios;
+    private IUsuarioDAO usuarioDAO;
+    private Usuario unUsuario;
+
+    public UsuarioBean(List<Usuario> listaUsarios, IUsuarioDAO usuarioDAO, Usuario unUsuario) {
+        this.listaUsarios = listaUsarios;
+        this.usuarioDAO = usuarioDAO;
+        this.unUsuario = unUsuario;
+    }
+    
+    
+    /**
+     * Creates a new instance of UsuarioBean
+     */
+    public UsuarioBean() {
+        IUsuarioDAO usuarioDAO= new UsuarioDaoImp();
+        listaUsarios=new ArrayList<>();
+    }
+    public Usuario validarUsuario(String nombreUsuario, String password){
+        return getUsuarioDAO().validarUsuario(nombreUsuario, password);
+    }
+    public Usuario obtenerUsuario (String nombreUsuario){
+        return getUsuarioDAO().obtenerUsuario(nombreUsuario);
+    }
+    public void modificarUsuario (Usuario unUsuario){
+        getUsuarioDAO().modificarUsuario(unUsuario);
+    }
+    public void eliminarUsuario (Usuario unUsuario){
+        getUsuarioDAO().eliminarUsuario(unUsuario);
+    }
+    public List <Usuario> obtenerListaUsuariosActivos(){
+        return getUsuarioDAO().obtenerListaUsuariosActivos();
+    }
+    public void agregarUsuario (Usuario unUsuario){
+        getUsuarioDAO().agregarUsuario(unUsuario);
+    }
+
+    /**
+     * @return the listaUsarios
+     */
+    public List<Usuario> getListaUsarios() {
+        return listaUsarios;
+    }
+
+    /**
+     * @param listaUsarios the listaUsarios to set
+     */
+    public void setListaUsarios(List<Usuario> listaUsarios) {
+        this.listaUsarios = listaUsarios;
+    }
+
+    /**
+     * @return the usuarioDAO
+     */
+    public IUsuarioDAO getUsuarioDAO() {
+        return usuarioDAO;
+    }
+
+    /**
+     * @param usuarioDAO the usuarioDAO to set
+     */
+    public void setUsuarioDAO(IUsuarioDAO usuarioDAO) {
+        this.usuarioDAO = usuarioDAO;
+    }
+
+    /**
+     * @return the unUsuario
+     */
+    public Usuario getUnUsuario() {
+        return unUsuario;
+    }
+
+    /**
+     * @param unUsuario the unUsuario to set
+     */
+    public void setUnUsuario(Usuario unUsuario) {
+        this.unUsuario = unUsuario;
+    }
+}
